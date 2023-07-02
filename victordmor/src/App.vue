2d03db5316f188afd78e0cc65699352c9f19af5c
Victor Moraes <vdmoraes94@gmail.com>
2023-07-02 00:22:59 -0300

    Starts redesign of my personal website using Vue

---
+++ victordmor/src/App.vue
@@ -0,0 +1,24 @@
+<!-- src/App.vue -->
+<template>
+  <div id="app">
+    <MainHeader />
+  </div>
+</template>
+
+<script>
+import MainHeader from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainHeader
+  }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ victordmor/src/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <header>
+    <div class="header-content">
+      <img :src="imageSrc" alt="Profile picture" />
+      <h1>{{ name }}</h1>
+    </div>
+  </header>
+</template>
+  
+<script>
+  import profilePic from '@/assets/personal_picture.png';
+
+  export default {
+    name: 'MainHeader',
+    data() {
+      return {
+        name: 'Victor D. Moraes',
+        imageSrc: profilePic
+      }
+    }
+  }
+</script>
+  
+<style scoped>
+header {
+  display: flex;
+  justify-content: center;
+  padding: 20px;
+  background-color: #f5f5f5;
+}
+
+.header-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+header img {
+  height: 100px;
+  width: 100px;
+  border-radius: 50%;
+}
+
+header h1 {
+  margin: 10px 0 0 0;
+  color: #333;
+}
+</style>
